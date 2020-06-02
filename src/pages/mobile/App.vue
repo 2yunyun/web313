e9394bf9061c69417721879efbc492a4d855d88c
zhangyun <lody2011@163.com>
2020-06-02 13:28:14 +0800

    配置多页面

---
+++ src/pages/mobile/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Mobile
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- template中使用环境变量 -->
+    API: {{api}}
+    <br>
+    {{name}}
+    <img alt="Vue logo"
+         src="@/assets/logo.png">
+    <svg-icon icon-class="password"></svg-icon>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -9,10 +15,35 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
+import axios from 'axios'
+
 export default {
-  name: 'Home',
+  name: 'home',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      api: process.env.VUE_APP_API,
+      name: ''
+    }
+  },
+  mounted() {
+    // js代码中使用环境变量
+    console.log("BASE_URL: ", process.env.BASE_URL);
+    console.log("VUE_APP_API: ", process.env.VUE_APP_API);
+
+    axios.get('/api/1').then(res => {
+      console.log('proxy:', res);
+    })
+
   }
 }
 </script>
+
+
+<style>
+.nihao {
+  background: blue;
+}
+</style>
